3071554d887e18f87267f28459431aacaed96339
speedyGonzales <yonchoy@abv.bg>
2015-06-27 14:13:23 +0300

    some boostraping baby

--- runs/bin/app.pl
+++ runs/bin/app.pl
@@ -1,4 +1,32 @@
 #!/usr/bin/env perl
+use strict;
+use warnings;
+use v5.018;
+
 use Dancer;
+use Dancer::Plugin::Database;
+use Dancer::Plugin::SimpleCRUD;
+
+use DBI;
+
 use runs;
+
+set template => 'template_toolkit';
+set log => "debug";
+set logger => "console";
+set warnings => 1;
+
+
+
+post '/login' => sub {
+    # Validate the username and password they supplied
+    if (params->{user} eq 'admin' && params->{pass} eq 'admin') {
+        session user => params->{user};
+        redirect params->{path} || '/';
+    } else {
+        redirect '/login?failed=1';
+    }
+};
+
+
 dance;
--- runs/lib/runs.pm
+++ runs/lib/runs.pm
@@ -1,10 +1,79 @@
 package runs;
+
+use strict;
+use warnings;
+use v5.018;
+
 use Dancer ':syntax';
 
 our $VERSION = '0.1';
 
+#the routing part
+
 get '/' => sub {
     template 'index';
 };
+
+get '/add' => sub {
+    template 'add';
+};
+
+get '/login' => sub {
+    template 'login';
+};
+
+get '/stats' => sub {
+    template 'stats';
+};
+
+#login form
+# post '/login' => sub {
+#     my $user = param 'user' ;
+#     my $pass = param 'pass' ;
+
+#     if (length($user)==0) {
+#         return template '/login', {
+#             show_warning => "Please enter username" };
+#     }
+#     if (length($pass)==0) {
+#         return template '/login', {
+#             show_warning => "Please enter password" };
+#     }
+
+#     my $dsn = "dbi:SQLite:dbname=foo.sqlite";
+
+#     my $dbh = DBI->connect($dsn, $user, $password, {
+#        PrintError       => 0,
+#        RaiseError       => 1,
+#        AutoCommit       => 1,
+#        FetchHashKeyName => 'NAME_lc',
+#     });
+
+#     my $sql = 'SELECT * FROM users WHERE username =? AND password= ?';
+#     my $sth = $dbh->prepare($sql);
+#     $sth->execute($user,$pass);
+
+#     @row = $sth->fetchrow_array();
+#     my $size = @row;
+
+#     if($size == 0){
+#          return template '/login', {
+#             show_warning => "There arent such user" };
+#     }
+
+#     my $sql = 'SELECT * FROM logs';
+#     my $sth = $dbh->prepare($sql);
+#     $sth->execute();
+
+#     @result = $sth->fetchrow_array();
+#     template 'stats', { data => $sth };
+# };
+
+# #login form
+# post '/' => sub {
+#     $sql = 'SELECT * FROM users WHERE username =? AND password= ?';
+#     $sth = $dbh->prepare($sql);
+#     $sth->execute($user,$pass);
+# };
 
 true;
